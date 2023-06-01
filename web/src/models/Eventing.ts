5823775b1716db2f9fc63189909e76c9ef863c8e
ldantas-s <lucianostudant@gmail.com>
2023-05-31 22:02:09 -0300

    feat(web): create Eventing class

---
+++ web/src/models/Eventing.ts
@@ -0,0 +1,17 @@
+type Callback = () => void;
+
+export class Eventing {
+  events: { [key: string]: Callback[] } = {};
+
+  on(eventName: string, callback: Callback): void {
+    this.events[eventName] = [...(this.events[eventName] || []), callback];
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) return;
+
+    handlers.forEach((callback) => callback());
+  }
+}
