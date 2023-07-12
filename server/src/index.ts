e5bd998d6781ba9401cfff41e95472ab78fb91d2
ldantas-s <lucianostudant@gmail.com>
2023-07-12 20:08:48 -0300

    feat(server): create a route login route file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,13 @@
 import express, { Request, Response } from 'express';
+import { router } from './routes/loginRoutes';
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-  res.send(`
-    <div>
-      <h1>Hi there</h1>
-    </div>
-  `);
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['lskdas'] }));
+app.use(router);
 
 app.listen(3000, () => {
   console.log('Listening on port: 3000');
---
+++ server/src/routes/loginRoutes.ts
@@ -0,0 +1,76 @@
+import { Router, Request, Response, NextFunction } from 'express';
+
+interface RequestWithBody extends Request {
+  body: { [key: string]: string | undefined };
+}
+
+function requireAtuh(req: Request, res: Response, next: NextFunction): void {
+  if (req.session && req.session.loggedIn) {
+    next();
+    return;
+  }
+
+  res.status(403).send('Not permitted');
+}
+
+const router = Router();
+
+router.get('/login', (req: Request, res: Response) => {
+  res.send(`
+    <form method="POST">
+      <div>
+        <label for="email">Email</label>
+        <input name="email" />
+      </div>
+      <div>
+        <label for="password">Password</label>
+        <input name="password" type="password" />
+      </div>
+      <button>submit</button>
+    </form>
+  `);
+});
+
+router.post('/login', (req: RequestWithBody, res: Response) => {
+  const body = req.body;
+  const email = body?.email;
+  const password = body?.password;
+
+  if (email && password && email === 'hi@hi.com' && password === 'pass') {
+    // mark this person logged in
+    req.session = { loggedIn: true };
+    // redirect them to the root route
+    res.redirect('/');
+  } else {
+    res.send('Invalid email or password');
+  }
+});
+
+router.get('/', (req: Request, res: Response) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(`
+      <section>
+        <h1>You are logged in</h1>
+        <a href='/logout'>Logout</a>
+      </section>
+    `);
+  } else {
+    res.send(`
+      <section>
+        <h1>You are not logged in</h1>
+        <a href='/login'>Login</a>
+      </section>
+    `);
+  }
+});
+
+router.get('/logout', (req: Request, res: Response) => {
+  req.session = undefined;
+  res.redirect('/');
+});
+
+router.get('/protected', requireAtuh, (req: Request, res: Response) => {
+  res.send('Welcome to protected route, logged in user!');
+});
+
+export { router };
