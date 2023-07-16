30b96e0448b030508655b7c854ab10aa6f5186c6
ldantas-s <lucianostudant@gmail.com>
2023-07-16 11:27:51 -0300

    feat(server): remove hard code metadata keys using enums

--- server/src/controllers/decorators/routes.ts
+++ server/src/controllers/decorators/routes.ts
@@ -1,12 +1,13 @@
 import 'reflect-metadata';
 
 import { Methods } from './Methods';
+import { MetadataKeys } from './MetadataKeys';
 
 function routerBinder(method: keyof typeof Methods) {
   return function (path: string) {
     return function (target: any, key: string, descriptor: PropertyDescriptor) {
-      Reflect.defineMetadata('path', path, target, key);
-      Reflect.defineMetadata('method', method, target, key);
+      Reflect.defineMetadata(MetadataKeys.path, path, target, key);
+      Reflect.defineMetadata(MetadataKeys.method, method, target, key);
     };
   };
 }
