dd50fa1e77fdf67690b45316a6e6591a9e1b17c2
ldantas-s <lucianostudant@gmail.com>
2023-07-17 23:59:29 -0300

    fix(server): update the type of descriptor to use decorators correctly

--- server/src/controllers/decorators/routes.ts
+++ server/src/controllers/decorators/routes.ts
@@ -1,11 +1,21 @@
 import 'reflect-metadata';
 
+import { RequestHandler } from 'express';
+
 import { Methods } from './Methods';
 import { MetadataKeys } from './MetadataKeys';
 
+interface RouterHandlerDescriptor extends PropertyDescriptor {
+  value?: RequestHandler;
+}
+
 function routerBinder(method: keyof typeof Methods) {
   return function (path: string) {
-    return function (target: any, key: string, descriptor: PropertyDescriptor) {
+    return function (
+      target: any,
+      key: string,
+      descriptor: RouterHandlerDescriptor
+    ) {
       Reflect.defineMetadata(MetadataKeys.path, path, target, key);
       Reflect.defineMetadata(MetadataKeys.method, method, target, key);
     };
